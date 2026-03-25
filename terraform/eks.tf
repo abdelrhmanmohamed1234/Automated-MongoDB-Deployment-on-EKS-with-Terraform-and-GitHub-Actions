1c9780fa4bb1673cd9d8b6fa6c4433bb48f67da7
root <root@controller.auto>
2026-03-26 00:05:01 +0200

    fixed

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.public[*].idi
+    subnet_ids = aws_subnet.public[*].id
     security_group_ids = [aws_security_group.node_sg.id]
   }
 
